6eebab528a161263041e8d19966ba74be3c65b07
Mohamed254 <khadarow12345@gmail.com>
2021-01-28 13:11:51 +0300

    Contact-Keeper

---
+++ client/src/context/contact/contactContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+//function of this is to initialize the whole context
+const contactContext = createContext();
+
+export default contactContext;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require('./config/db');
+
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false}));
+app.get("/", (req, res) => res.send("Hello world"));
+
+//Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+//console.log(`server started on port ${PORT}`);
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
